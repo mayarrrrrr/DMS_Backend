e33219fd77addcb211e468dd0f3328d758617b43
mujahidina <mujahidabdi5070@gmail.com>
2024-11-12 21:08:14 +0300

    fixed serialization

--- models.py
+++ models.py
@@ -8,10 +8,10 @@ db = SQLAlchemy()
 class User(db.Model, SerializerMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    type = db.Column(db.String(50), nullable=False)  # 'reporter' or 'rescuer'
+    name = db.Column(db.String(), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    type = db.Column(db.String(), nullable=False)
     
     @validates('password')
     def validate_password(self, key, password):
@@ -27,32 +27,56 @@ class User(db.Model, SerializerMixin):
 
     reports = db.relationship('Disaster', backref='user', lazy=True)  
     
+    
+    serialize_rules = ('-reports.user',)  
+    
     def __repr__(self):
         return f'<User {self.name}>'
 
 class Rescuer(db.Model, SerializerMixin):
     __tablename__ = 'rescuer'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # firefighter, etc.
+    name = db.Column(db.String(), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    role = db.Column(db.String(), nullable=False)  # firefighter, etc.
     disasters = db.relationship('Disaster', backref='rescuer', lazy=True)  
+
+    
+    serialize_rules = ('-disasters.rescuer',)  
 
     def __repr__(self):
         return f'<Rescuer {self.name}>'
 
+
+
 class Disaster(db.Model, SerializerMixin):
     __tablename__ = 'disaster'
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(500), nullable=False)  # Description of the disaster
+    description = db.Column(db.String(), nullable=False)  
     date_reported = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Link to reporter
-    rescuer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Link to rescuer
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
+    rescuer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
     
     user = db.relationship('User', foreign_keys=[user_id], backref='reported_disasters')
     rescuer = db.relationship('User', foreign_keys=[rescuer_id], backref='responded_disasters')
+
     
+    serialize_rules = ('-user.reported_disasters', '-rescuer.responded_disasters')
+
     def __repr__(self):
         return f'<Disaster {self.description} - Reported by {self.user.name}>'
+    
+
+
+class Reporter(db.Model, SerializerMixin):
+    __tablename__ = 'reporter'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    
+    
+    serialize_rules = ('-reports.user',)  
+    
+    def __repr__(self):
+        return f'<Reporter {self.id} - {self.name}>'
+
